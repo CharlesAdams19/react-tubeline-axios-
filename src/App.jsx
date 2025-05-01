39c6583bca221cc7de0beb68bf346d9dd0d94b3f
Charles Adams 19 <adamscra19@gmail.com>
2025-05-01 14:10:53 +0100

    code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
-// src/App.jsx
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 const App = () => {
+  const [tubeLines, setTubeLines] = useState([]);
+
+  useEffect(() => {
+    async function getTubeData() {
+      try {
+        const { data } = await axios.get('https://api.tfl.gov.uk/line/mode/tube/status');
+        setTubeLines(data);
+      } catch (error) {
+        console.log('Error fetching tube lines:', error);
+      }
+    }
+
+    getTubeData();
+  }, []);
 
   return (
-    <h1>Hello world!</h1>
+    <div>
+      <h1>London Underground Status</h1>
+      <ul>
+        {tubeLines.map(line => (
+          <li key={line.id}>
+            {line.name} {line.lineStatuses[0]?.statusSeverityDescription}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
-}
+};
 
-export default App
+export default App;
